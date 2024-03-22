d18dda815a3d6e7ca5702c75420e309f1d3cf5e7
umarfaisal295c <umarfaisalumarfaisal447@gmail.com>
2024-03-22 23:53:08 +0500

    firebase

--- learnFireBase/src/App.jsx
+++ learnFireBase/src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Signup from "./components/Signup";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>FireBase</h1>
+      <Signup />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ learnFireBase/src/firebase/FireBaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB-L_lt3h4cjA4euuJ13_Hs6bLeHpxzJqA",
+  authDomain: "learnfirebase-80abf.firebaseapp.com",
+  projectId: "learnfirebase-80abf",
+  storageBucket: "learnfirebase-80abf.appspot.com",
+  messagingSenderId: "481994676848",
+  appId: "1:481994676848:web:0f68ffe01adb36cd6a32b5",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
