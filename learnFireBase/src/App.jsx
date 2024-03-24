6132862ec9146296d82ed74ac39a0caa441d70be
umarfaisal295c <umarfaisalumarfaisal447@gmail.com>
2024-03-24 15:12:01 +0500

    FirsStore Database

--- learnFireBase/src/App.jsx
+++ learnFireBase/src/App.jsx
@@ -1,18 +1,33 @@
-import "./App.css";
-import Google from "./components/Google";
-import Signin from "./components/Signin";
-import Signup from "./components/Signup";
-
-function App() {
+import React from "react";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { app } from "./firebase/FireBaseConfig";
+const fireStore = getFirestore(app);
+const App = () => {
+  const writedata = async () => {
+    const result = await addDoc(collection(fireStore, "cites"), {
+      name: "Lahore",
+      pinCode: 42,
+      lat: 1234,
+      lang: 1212,
+    });
+    console.log("Result", result);
+  };
+  // create a function for subCollection
+  const makeSubCollection = async () => {
+   const data= await addDoc(collection(fireStore, "cites/H83js7LDvBhcFUvrOQWJ/places"), {
+      name: "Place 1",
+      disc: "Awsome Place 1",
+      date: Date.now(),
+    });
+    console.log("Data",data);
+  };
   return (
     <>
-      <h1>FireBase</h1>
-      <Signup />
-      <br />
-      <Signin />
-      <Google />
+      <h1>FireStore</h1>
+      <button onClick={writedata}>Put data</button>
+      <button onClick={makeSubCollection}>Put Subdata</button>
     </>
   );
-}
+};
 
 export default App;
